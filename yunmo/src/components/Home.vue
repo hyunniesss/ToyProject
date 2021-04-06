64bb48d44ff456c518d51dc1c3c8235f0c488312
hyunhee <wjdgusgml997@naver.com>
2021-04-06 17:47:49 +0900

    [+]UPD yunmo router-view

---
+++ yunmo/src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div
+        style="
+      position: fixed;
+      height: 75px;
+      padding: 1rem;
+      color: black;
+      background-color: rgba(0, 0, 0, 0.5);
+      font-weight: bold;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 5;
+    ">야아아아ㅏ</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
